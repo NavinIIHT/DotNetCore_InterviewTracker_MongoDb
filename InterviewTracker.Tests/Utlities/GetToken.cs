c5269817adeb518b40f8e6ba1c702120320b96cd
Uma Kumar <umakumarsingh@gmail.com>
2021-10-27 12:39:46 +0530

    Updatetest

---
+++ InterviewTracker.Tests/Utlities/GetToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewTracker.Tests.Utlities
+{
+    public class GetToken
+    {
+        public string token { get; set; }
+        public string error { get; set; }
+    }
+}
